54ca21460bb2ba11467e0b222efadb985e4c05d9
hnsaini94 <hnsaini94@gmail.com>
2017-02-10 15:39:10 -0500

    Update closestnumbers.java

--- closestnumbers.java
+++ closestnumbers.java
@@ -1,3 +1,7 @@
+/*This programs take two inputs:
+	1. Input as array(array can be of any length, numbers seperated with spaces).
+	2. Target number.
+  The program will find two closest numbers to the target number from the array input.*/
 import java.util.Scanner;
 
 public class closestnumbers {
@@ -7,19 +11,21 @@ public class closestnumbers {
 		int frs,sec;
 		Scanner scan= new Scanner(System.in);
 		System.out.println("Enter the array");
-		String s= scan.nextLine();
-		String[] ch= s.split(" ");
+		String s= scan.nextLine();		// As user can enter any length input, it is saved as string
+		
+		String[] ch= s.split(" ");		//Here the string is splitted into substrings
 		int[] arr= new int[ch.length];
 		for(int i=0;i<ch.length;i++)
-			arr[i]= Integer.parseInt(ch[i]);
+			arr[i]= Integer.parseInt(ch[i]);//Here the substrings are parsed to int
 		System.out.println("Enter Target");
-		int t= scan.nextInt();
-		frs=arr[0];
-		sec=arr[1];
+		int t= scan.nextInt();	
+		
+		frs=(arr[0]<arr[1])?arr[0]:arr[1];
+		sec=(arr[0]<arr[1])?arr[1]:arr[0];
 		for(int i=2;i<arr.length;i++){
 			if(Math.abs(t-arr[i])<=Math.abs(t-frs))
 			{
-				sec=(sec<frs)?sec:frs;
+				sec=frs;
 				frs=arr[i];
 			}
 			else if(Math.abs(t-arr[i])<=Math.abs(t-sec)){
@@ -27,7 +33,7 @@ public class closestnumbers {
 			}
 		}
 		scan.close();
-		System.out.println(frs + " " + sec);
+		System.out.println("First Number="frs + " " +"Second Number+" sec);
 	}
 
 }
